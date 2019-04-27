87d28b1d09dfbdb50a3f6dbaeda7250dc7390c58
Alan Vardy <alanvardy@gmail.com>
2019-04-26 20:15:24 -0600

    wip

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -1,2 +1,19 @@
-config :live_view_todo, LiveViewTodoWeb.Endpoint,
-secret_key_base: System.get_env("SECRET_KEY_BASE")
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you'll likely want to automate and keep away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or yourself later on).
+config :live_view_todos, LiveViewTodosWeb.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :live_view_todos, LiveViewTodos.Repo,
+  username: "postgres",
+  password: "postgres",
+  database: "live_view_todos_prod",
+  pool_size: 15
